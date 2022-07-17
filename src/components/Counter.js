713cc90d11e89f012e64841bb575f20704d16238
geoffreyogembo <geoffreyogembo06@gmail.com>
2022-07-17 15:16:39 -0400

    Done.

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,10 +1,15 @@
 import React, { useState } from "react";
 
+
 function Counter() {
   const [count, setCount] = useState(0);
 
   function increment() {
-    setCount(count + 1);
+    // setCount(count + 1);
+    setCount((currentCount) => currentCount + 1);
+    setCount((currentCount) => currentCount + 1);
+    // using a callback function to update the state
+    // setting state is an asynchronous operation
   }
 
   return <button onClick={increment}>I have been clicked {count} times</button>;
